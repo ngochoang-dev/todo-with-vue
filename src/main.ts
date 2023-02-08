883a374d79e70eaf29aee5451929f0506482147b
ngochoang.dev <55541339+ngochoang-monster@users.noreply.github.com>
2023-02-08 23:42:40 +0700

    task: init app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "./assets/css/index.scss";
+import App from "./App.vue";
+import router from "./router/index";
+import { useAuthStore } from "./store/auth";
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(createPinia()).mount("#app");
+
+const useStore = useAuthStore();
+
+console.log(useStore.$state.authenticated);
+
+router.beforeEach(async (to, from) => {
+  const authenticated = useStore.$state.authenticated;
+
+  if (!authenticated && to.name !== "Login") {
+    return { name: "Login" };
+  }
+
+  if (authenticated && to.name === "Login") {
+    return { name: "Home" };
+  }
+});
